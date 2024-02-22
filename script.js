6bfb5d765a6c2403760de019a3ce81c5b5d7cbf6
Perla Mendoza <149718587+Pmendoza07@users.noreply.github.com>
2024-02-22 15:04:42 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+"use scrict";
+
+function game (){
+
+if (confirm("like people")){
+alert("incorrect!!!!!your not special!");
+}else{
+    alert("correct!!!!!your special!");
+}
+
+if (confirm("I like Ms.nell")){
+    alert("correct!!!!!your special!");
+}else{
+    alert("incorrect!!!!!your not special!");
+}
+
+if (confirm("Eve is a giant <3")){
+    alert("correct!!!!!We all know it but she wont admit it!");
+}else{
+    alert("incorrect!!!!! you need to go to the eye doctor! (214) 645-0950 in case your cant find it! :)");
+}
+
+
+//5 questions
+if (prompt("Do we need money? in others words we are all broke right?;)") === "yes".toLowerCase || "y".toLowerCase){
+    console.log("question one response is yes");
+}
+
+if (prompt("should we dip school?") === "no".toLowerCase || "n".toLowerCase){
+    console.log("question one response is no");
+}
+
+if (prompt(" Is eve a monster?") === "yes".toLowerCase || "y".toLowerCase){
+    console.log("question one response is yes");
+}
+
+if (prompt(" Is eve an ADDICT to her phone?") === "yes".toLowerCase || "y".toLowerCase){
+    console.log("question one response is yes");
+}
+if (prompt("Does jordan look grumpy? ") === "yes".toLowerCase || "y".toLowerCase){
+    console.log("question one response is yes");
+}
+
+
+}
+function getToKnowYou(){
+    let username = prompt("What is your name?");
+    alert("hello wierdooo0O0000Oo"+ username + "welcome to my site!")
+    alert(username+ ", I hope you enjoy the rest of my site");
+}
